6ce0bea23bdffd5a8b6b1bd09f87b70dbb8b1da9
Faizal Khalifa <faizalfkhalifa786@gmail.com>
2022-09-27 18:12:30 +0530

    edit remove

--- src/components/CommentsOptions.jsx
+++ src/components/CommentsOptions.jsx
@@ -27,7 +27,7 @@ const CommentsOptions = ({ _id, setFetch }) => {
       <BsThreeDotsVertical className="cursor-pointer" onClick={onThreeDotsClick} />
       {option && (
         <div className="absolute bottom-4 left-2 border px-3 py-2 flex gap-1 flex-col">
-          <button>Edit</button>
+          {/* <button>edit</button> */}
           <div className="border-b"></div>
           <button onClick={onDeleteComment}>remove</button>
         </div>
